5994149f73e1bede79399456b2327794bf03e13d
Olof Olsson <borsna@gmail.com>
2015-05-06 19:10:27 +0200

    fix spaces

--- build.py
+++ build.py
@@ -1,4 +1,7 @@
 import os;
+#Build script
+#takes all external css and js-files in one html-file
+
 INPUT='vocable.html'
 OUTPUT='build/vocable.html'
 
@@ -7,35 +10,33 @@ if not os.path.exists('build'):
 
 file = open(OUTPUT, "w")
 
-def find_between( s, first, last ):
+def find_between(s, first, last):
   try:
-    start = s.index( first ) + len( first )
-    end = s.index( last, start )
+    start = s.index(first) + len(first)
+    end = s.index(last, start)
     return s[start:end]
   except ValueError:
     return ""
 
-def get_file_content( path ):
+def get_file_content(path):
   f = open(path, 'r')
   return f.read()
-	
+
 def js_include( line ):
   path = find_between(line, 'src="', '"')
   js = get_file_content(path)
   return '<script type="text/javascript">\n'+js+'\n</script>\n'
-  
+
 def css_include( line ):
   path = find_between(line, 'href="', '"')
   css = get_file_content(path)
   return '<style>\n'+css+'\n</style>\n'
-  
+
 with open(INPUT) as input_file:
   for line in input_file:
     if line.strip().startswith('<script src='):
-      file.write(js_include( line.strip() ))
-    elif line.strip().startswith( '<link rel="stylesheet"' ):
-      file.write(css_include( line.strip() ))
+      file.write(js_include(line.strip()))
+    elif line.strip().startswith('<link rel="stylesheet"'):
+      file.write(css_include(line.strip()))
     else: 
-      file.write( line )
-	  
-	  
+      file.write(line)
