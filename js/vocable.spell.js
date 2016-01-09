12ba3b6abfa85f0e5da4a00d8c6e457394464afd
Per Lindstrand <fpj@spotify.com>
2016-01-09 23:59:13 +0100

    Use Math.min instead of own stuff

--- js/vocable.spell.js
+++ js/vocable.spell.js
@@ -10,16 +10,6 @@ function editDistance(s1, s2) {
         return arr;
     }
 
-    // Returns the minimum of all supplied argument.
-    function minArgument() {
-        var min = arguments[0];
-        for (var i = 1; i < arguments.length; i++) {
-            var arg = arguments[i];
-            min = arg < min ? arg : min;
-        }
-        return min;
-    }
-
     var d = new2DArray(s1.length + 1, s2.length + 1);
     for (var i = 0; i <= s1.length; i++) {
         d[i][0] = i;
@@ -30,12 +20,12 @@ function editDistance(s1, s2) {
     for (var i = 1; i <= s1.length; i++) {
         for (var j = 1; j <= s2.length; j++) {
             var cost = s1[i-1] != s2[j-1];
-            d[i][j] = minArgument(
+            d[i][j] = Math.min(
                 d[i-1][j] + 1,
                 d[i][j-1] + 1,
                 d[i-1][j-1] + cost);
             if (i > 1 && j > 1 && s1[i-1] == s2[j-2] && s1[i-2] == s2[j-1]) {
-                d[i][j] = minArgument(
+                d[i][j] = Math.min(
                     d[i][j],
                     d[i-2][j-2] + cost);
             }
